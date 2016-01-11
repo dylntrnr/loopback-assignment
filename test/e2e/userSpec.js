2a8bf9d5632f06075cd1eec2460b6a27e36b37d6
dylan turner <dylan@doxy.me>
2016-01-10 19:30:03 -0600

    setup model and relations from all tests passing

--- test/e2e/userSpec.js
+++ test/e2e/userSpec.js
@@ -22,6 +22,18 @@ function createUser() {
   });
 }
 
+function createBook(name) {
+  return app.models.book.create({
+    name: name || chance.word()
+  });
+}
+
+function createLibrary() {
+  return app.models.library.create({
+    name: chance.word()
+  });
+}
+
 function getUserObj() {
   return {
     email: chance.email(),
@@ -145,6 +157,59 @@ describe('user', function() {
         .then(function(res) {
           expect(res.body.email.unique).to.equal(true);
         });
+    });
+  });
+  // Implement hasManyThrough relation with library and books
+  describe('PUT /api/users/{id}/libraries/rel/{fk}', function() {
+    var context = {};
+    beforeEach(function() {
+      return clearDB()
+      .then(createUser)
+      .then(function(user) {
+        context.user = user;
+        return user.createAccessToken(DEFAULT_TTL);
+      }).then(function(accessToken) {
+        context.accessToken = accessToken;
+      })
+      .then(createLibrary)
+      .then(function(library) {
+        context.library = library;
+      });
+    });
+    it('should create a book when put', function() {
+      var bookName = chance.word();
+      return request(app)
+      .put('/api/users/' + context.user.id + '/libraries/rel/' + context.library.id)
+      .set({'authorization': context.accessToken.id})
+      .send({name: bookName})
+      .expect(200)
+      .then(function(res) {
+        expect(res.body.name).to.equal(bookName);
+      });
+    });
+    it('should create two books with two puts', function() {
+      var bookName = chance.word();
+      var bookName2 = chance.word();
+      createBook(bookName2);
+      return request(app)
+      .put('/api/users/' + context.user.id + '/libraries/rel/' + context.library.id)
+      .set({'authorization': context.accessToken.id})
+      .send({name: bookName})
+      .expect(200)
+      .then(function(){
+        return request(app)
+        .put('/api/users/' + context.user.id + '/libraries/rel/' + context.library.id)
+        .set({'authorization': context.accessToken.id})
+        .send({
+          name: bookName2,
+          userId: context.user.id,
+          libraryId: context.library.id
+        })
+        .expect(200)
+        .then(function(res) {
+          expect(res.body.name).to.not.equal(bookName);
+        });
+      });
     });
   });
 });
